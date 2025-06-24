cc23b43bb62605f1c7c59b5173885d6c60f92be2
Win-unifi <vincenzo.valori@unifi.it>
2025-06-24 18:05:59 +0200

    Un macello iniziale

---
+++ contest/__init__.py
@@ -0,0 +1,99 @@
+from otree.api import *
+
+doc = """
+Your app description
+"""
+
+class C(BaseConstants):
+    NAME_IN_URL = 'contest'
+    PLAYERS_PER_GROUP = None
+    NUM_ROUNDS = 2
+    ENDOWMENT = Currency(10)
+    COST_PER_TICKET = Currency(0.50)
+    PRIZE = Currency(8)
+
+class Subsession(BaseSubsession):
+    is_paid = models.BooleanField()
+
+    def setup_round(self):
+        self.is_paid = True
+        for group in self.get_groups():
+            group.setup_round()
+
+    def compute_outcome(self):
+        for group in self.get_groups()
+            group.compute_outcome()
+
+class Group(BaseGroup):
+    prize = models.CurrencyField()
+
+    def setup_round(self):
+        self.prize = C.PRIZE
+        for player in self.get_players():
+            player.setup_round()
+
+    def compute_outcome(self):
+        total = sum(player.tickets_purchased for player in self..get_players())
+        for player in self.get_players():
+            try:
+                player.prize_won = player.tickets_purchased / total
+            except ZeroDivisionError:
+                player.prize_won = 1 / len(self.get_players())
+
+
+
+class Player(BasePlayer):
+    endowment = models.CurrencyField()
+    cost_per_ticket = models.CurrencyField()
+    tickets_purchased = models.IntegerField()
+
+    def setup_round(self):
+        self.endowment = C.ENDOWMENT
+        self.cost_per_ticket = C.COST_PER_TICKET
+
+    @property
+    def coplayer(self):
+        return self.group.get_player_by_id(3 - self.id_in_group)
+
+#Creates a global scope function alternative to having the SetupRound(WaitPage)
+#def creating_session(subsession):
+#    subsessionsetup_round().
+
+# PAGES
+
+class SetupRound(WaitPage):
+    wait_for_all_groups = True
+    @staticmethod
+    def after_all_players_arrive(subsession):
+        subsession.setup_round()
+
+class Intro(Page):
+    pass
+
+class Decision(Page):
+    form_model = "player"
+    form_fields = ["tickets_purchased"]
+
+class DecisionWaitPage(WaitPage):
+    pass
+
+class Results(Page):
+    pass
+# Not good. Definition TOO LONG. Solution is easy: see Results.html
+#    @staticmethod
+#    def vars_for_template(player):
+#        return {
+#            "coplayer_purchase": player.group.get_player_by_id(3 - player.id_in_group).tickets_purchased
+#       }
+
+class EndBlock(Page):
+    pass
+
+page_sequence = [
+    SetupRound,
+    Intro,
+    Decision,
+    DecisionWaitPage,
+    Results,
+    EndBlock
+]
